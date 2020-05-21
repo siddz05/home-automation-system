089eb2a584967a6c59ae9d563dc8fb96dbfac87c
Sid <siddharth.dwivedi@cleartrip.com>
2020-05-21 23:56:59 +0530

    initial commit

---
+++ src/main/java/com/signz/homeautomation/HomeAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.signz.homeautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/signz/homeautomation/HomeAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signz.homeautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
