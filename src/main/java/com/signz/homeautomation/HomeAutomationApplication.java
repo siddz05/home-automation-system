19c638c4d7e1318de47fe8419321e2d319f5359e
Sid <siddharth.dwivedi@cleartrip.com>
2020-05-22 01:11:08 +0530

    health check api + package structure added

--- src/main/java/com/signz/homeautomation/HomeAutomationApplication.java
+++ src/main/java/com/signz/homeautomation/HomeAutomationApplication.java
@@ -1,13 +1,28 @@
 package com.signz.homeautomation;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@RequestMapping
+@RestController
+@Log4j2
 public class HomeAutomationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomeAutomationApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(HomeAutomationApplication.class, args);
+        log.info("Home Automation System Server Started ");
+    }
+
+    @GetMapping(value = {"/heath-check", "/"})
+    public String healthCheck() {
+        return "Home Automation System Is Up & Running !";
+    }
 
 }
